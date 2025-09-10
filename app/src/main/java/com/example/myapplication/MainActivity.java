fbdd6d28597c5c08e9390dfc86e67713f666b517
Kartikssethi <kartikssethi@gmail.com>
2025-09-10 22:23:39 +0530

    Fast api implementation

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,48 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
+    private TextView Helloworld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Helloworld = findViewById(R.id.Helloworld);
+
+        fetchMessageFromAPI();
+    }
+
+    private void fetchMessageFromAPI() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:8000/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ApiService apiService = retrofit.create(ApiService.class);
+        apiService.getHello().enqueue(new Callback<HelloResponse>() {
+            @Override
+            public void onResponse(Call<HelloResponse> call, Response<HelloResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    Helloworld.setText(response.body().message);
+                } else {
+                    Helloworld.setText("Error: No message");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<HelloResponse> call, Throwable t) {
+                Helloworld.setText("Failed to connect");
+                Log.e("FastAPI", "API call failed", t);
+            }
         });
     }
 }
